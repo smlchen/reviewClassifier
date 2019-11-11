99de61ed63aec0f95d9b70b3d8ed8edd45c3bb4f
Samuel Chen <samuelchen@Samuels-MacBook-Pro.local>
2019-11-10 20:56:39 -0800

    reorganized and wrapped up Phase I & II

--- get_data.py
+++ get_data.py
@@ -6,22 +6,13 @@ Created on Fri Nov  8 04:12:16 2019
 """
 # 1. download all the data as zip
 # 2. change the directory to the data location (make sure book is in a different dir)
-# Data File Structure:
-# large_data
-# - Books_5.json.gz (6.60GB)
-# - Clothing_Shoes_And_Jewelry_5.json.gz (1.17GB)
-# - Electronics_5.json.gz (1.16GB)
-# - Home_And_Kitchen_5.json.gz (909 MB)
-# - Movies_And_TV_5.josn.gz (754 MB)
-# - Kindle_Store_5.json.gz (513 MB)
-# data
-# - *everything else
+
 import glob, os, pandas as pd
 import random
 os.chdir(r"D:/data_zip")     # path of categories
 i =1
 dff= pd.DataFrame()
-	
+
 for file in glob.glob("*.gz"):
     print(file)
     data_df = pd.read_json(file, lines=True, compression='infer')    # read json file
@@ -33,7 +24,7 @@ for file in glob.glob("*.gz"):
     i+=1
     dff=dff.append(data_df)
     print("---------")
-    
+
 dff.reset_index(inplace=True , drop = True)
 # large file implementation
 # computer couldnt handle the big files, so taking them in chunks
@@ -43,7 +34,7 @@ for file in glob.glob("*.gz"):
     print(file)
     for chunk in pd.read_json(file, chunksize=200000,lines=True, compression='infer'):
         print(type(chunk))
-        df = chunk  
+        df = chunk
         print(df.shape)
         df=df.drop_duplicates(subset='reviewText')
         print(df.shape)
