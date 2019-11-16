7443856f809bff43de08592b4d9b865f1057a0ff
Samuel Chen <samuelchen@Samuels-MacBook-Pro.local>
2019-11-16 10:13:10 -0800

    further text cleaning and reduced features of tfidf down to 1483.

--- tf_idf.py
+++ tf_idf.py
@@ -7,7 +7,7 @@ from pprint import pprint
 #function for computing a tf-idf matrix given training/test data
 def computeTFIDF(df):
     collection = df['cleanText'].astype(str).tolist()
-    tfidf = TfidfVectorizer(token_pattern=r'(?u)\b[A-Za-z]+\b')     #regrex ignores any that contains numbers
+    tfidf = TfidfVectorizer(token_pattern=r'(?u)\b[A-Za-z]+\b', max_features=1500, min_df=20)     #regrex ignores any that contains numbers
     features = tfidf.fit_transform(collection)
     matrix = pd.DataFrame(features.todense(), columns=tfidf.get_feature_names())    #panda dataFrame representation of the matrix
     return matrix
@@ -16,6 +16,7 @@ amazon = pd.read_csv('./cleanAmazon_reviewtextOnly.csv', header=0)
 tfidf = computeTFIDF(amazon)
 tfidf['output1'] = amazon['Category']
 tfidf['output2'] = amazon['overall']
+tfidf.to_csv(r'./tfidf.csv', index=False)
 tfidf = tfidf.sample(frac=1, random_state=100).reset_index(drop=True)               #shuffle
 
 # splittig into training and test data using 5-fold cross validation
@@ -27,8 +28,8 @@ for train_index, test_index in kf.split(tfidf):
     train, test = tfidf.iloc[train_index], tfidf.iloc[test_index]
     train = train.reset_index(drop=True)
     test = test.reset_index(drop=True)
-    # print(train.shape)
-    # print(test.shape)
+    print(train.shape)
+    print(test.shape)
     train.to_csv(r'./split/' + str(i) + r'/training_data.csv', index=False)
     test.to_csv(r'./split/' + str(i) + r'/test_data.csv', index=False)
     i += 1
