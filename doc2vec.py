918a6905b59560ee13393aff1c8133dcb19c653d
Samuel Chen <samuelchen@Samuels-MBP.localdomain>
2019-11-17 22:37:53 -0800

    fixed some bugs in doc2vec.

--- doc2vec.py
+++ doc2vec.py
@@ -5,13 +5,13 @@ import pandas as pd
 import json
 
 amazon = pd.read_csv('./cleanAmazon_reviewtextOnly.csv', header=0)
-amazon['cleanText'].dropna(inplace=True)            #ugh
-data = amazon['cleanText'].tolist()
-
+doc2vec = amazon.dropna()            #getting rid of null values in cleantextd
+doc2vec = doc2vec.reset_index(drop=True)
+data = doc2vec['cleanText'].tolist()
 tagged_data = [TaggedDocument(words=word_tokenize(_d),
         tags=[str(i)]) for i, _d in enumerate(data)]
-max_epochs = 10
-vec_size = 1483
+max_epochs = 100
+vec_size = 50
 alpha = 0.025
 
 model = Doc2Vec(size=vec_size,
@@ -21,7 +21,6 @@ model = Doc2Vec(size=vec_size,
                 dm =1)
 
 model.build_vocab(tagged_data)
-
 for epoch in range(max_epochs):
     print('iteration {0}'.format(epoch))
     model.train(tagged_data,
@@ -34,5 +33,9 @@ for epoch in range(max_epochs):
 
 # to find most similar doc using tags
 similar_doc = model.docvecs.most_similar('2')
-print(similar_doc)
-print(model.docvecs[0])
+# print(similar_doc)
+
+#get all vectors
+vectors = model.docvecs.doctag_syn0.tolist()
+doc2vec['Vector'] = vectors
+doc2vec.to_csv(r'doc2vec.csv', index=False)
