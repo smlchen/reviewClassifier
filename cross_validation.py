532f2af73fd1f920a175cc976a8554218ba9eecf
slampham <43860771+slampham@users.noreply.github.com>
2019-11-23 13:51:08 -0800

    Completed ROC curves for one K-fold only. It needs to be updated to take the average ROC curves for multiple K-folds. I don't know how to take the average of serveral dictionaries (i.e. a fpr dictionary from each K-fold and taking their averages).

--- cross_validation.py
+++ cross_validation.py
@@ -1,88 +1,104 @@
+from os.path import isfile
+
+import numpy as np
 from sklearn.metrics import roc_curve, auc
 from sklearn.preprocessing import label_binarize
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from tensorflow_core.python.keras.models import load_model
 
-from build_model import build_model
+
+def getData(fold="1"):
+    df_train = pd.read_csv("split_tfidf/" + fold + "/training_data.csv")
+    df_test = pd.read_csv("split_tfidf/" + fold + "/test_data.csv")
+
+    X_train = np.asarray(df_train.iloc[:, :-2])
+    y_train = df_train.iloc[:, -1]
+    X_test = np.asarray(df_test.iloc[:, :-2])
+    y_test = df_test.iloc[:, -1]
+
+    y_train = label_binarize(y_train, classes=[1, 2, 3, 4, 5])
+    y_test = label_binarize(y_test, classes=[1, 2, 3, 4, 5])
+
+    results = pd.read_csv(r'grid_search_results/' + 'results.csv')
+    results = results[results.predictor_type != 'log_regression']
+
+    return X_train, y_train, X_test, y_test, results
 
 
-def plotROC(fpr, tpr, roc_auc):
+def plotROC(fpr, tpr, auc):
     plt.figure()
-    for i in range(5):
-        plt.plot(fpr[i], tpr[i], label='ROC curve: Rating {0} (area = {1:0.2f})'.format(i + 1, roc_auc[i]))
+    for rating in range(5):
+        plt.plot(fpr[rating], tpr[rating], label='ROC curve: Rating {0} (area = {1:0.2f})'.format(rating + 1, auc[rating]))
 
     plt.plot([0, 1], [0, 1], 'k--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title('ROC Curves of Ratings')
+    plt.title('ROC Curves for Ratings')
     plt.legend(loc="lower right")
     plt.show()
 
 
+def get_roc_auc(y_test, y_predict):
+    fpr = dict(); tpr = dict(); roc_auc = dict()
+    for rating in range(5):
+        fpr[rating], tpr[rating], _ = roc_curve(y_test[:, rating], y_predict[:, rating])
+        roc_auc[rating] = auc(fpr[rating], tpr[rating])
+    return fpr, tpr, roc_auc
+
+
+def calcAvgOfDiffROCAUC(fpr_list, tpr_list, auc_list):
+    # FIXME: stub
+    return avg_fpr, avg_tpr, avg_auc
+
+
+def getROCForAllFolds(best_model):
+    model = load_model(best_model)
+
+    fpr_list = []; tpr_list = []; auc_list = []
+    for fold in range(1, 6):
+        _, _, X_test, y_test, results = getData(str(fold))
+        y_predict = model.predict(X_test)
+        fpr, tpr, auc = get_roc_auc(y_test, y_predict)
+
+        # fpr = dict of ndarray. auc = dict of floats
+        fpr_list.append(fpr.values());
+        tpr_list.append(tpr.values());
+        auc_list.append(auc.values())
+
+    # FIXME: All fpr, tpr, auc dictionaries from each K-fold are inserted into their corresponding
+    # FIXME: list, but I don't know how to take the mean of each index from several dictionaries.
+    avg_fpr, avg_tpr, avg_auc = calcAvgOfDiffROCAUC(fpr_list, tpr_list, auc_list)
+    plotROC(avg_fpr, avg_tpr, avg_auc)
+
+
 def cross_validate_tfidf():
     """ Referenced implementation: https://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html """
+    _, _, X_test, y_test, results = getData(fold="1")
 
-    df_train = pd.read_csv("split_tfidf/1/training_data.csv")  # FIXME: Only using 1st K-Fold. Use all later when working
-    df_test = pd.read_csv("split_tfidf/1/test_data.csv")
+    # Find best model to work with against the 1st fold of data.
+    best_model = ""
+    most_AUC = 0
+    for _, row in results.iterrows():
+        file_name = r'grid_search_results/' + row['file_name']
+        if not isfile(file_name):
+            continue
 
-    X_train = df_train.iloc[:, :-2]
-    y_train = df_train.iloc[:, -1]
-    X_test = df_test.iloc[:, :-2]
-    y_test = df_test.iloc[:, -1]
+        model = load_model(file_name)
+        y_predict = model.predict(X_test)
+        fpr, tpr, auc = get_roc_auc(y_test, y_predict)
 
-    y_train = label_binarize(y_train, classes=[1, 2, 3, 4, 5])
-    y_test = label_binarize(y_test, classes=[1, 2, 3, 4, 5])
+        if most_AUC < sum(auc.values()):
+            most_AUC = sum(auc.values())
+            best_model = file_name
 
-    df = pd.read_csv("grid_search_results/results.csv")
-    for para in df.iterrows():
-        model = build_model(para['layers'], para['nodes'], X_train.shape[1], 5, loss=para['loss'], clipnorm=1.0)
-        # FIXME: NaN values when you put smaller batchsize / more epoches. y_score explodes in value, not sure why?
-        # FIXME: However, clipnorm should remove them, so I'm not sure why it stil explodes
-        history = model.fit(X_train, y_train, epochs=50, batch_size=32, verbose=2)
-        y_score = model.predict(X_test)
-
-        fpr = dict()
-        tpr = dict()
-        roc_auc = dict()
-        for i in range(5):
-            fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
-            roc_auc[i] = auc(fpr[i], tpr[i])
-
-        plotROC(fpr, tpr, roc_auc)
+    plotROC(fpr, tpr, auc)
+    # FIXME: need to average the ROC / AUC curves with the different folds.
+    # FIXME: getROCForAllFolds(best_model)
 
 
 if __name__ == '__main__':
     cross_validate_tfidf()
-
-
-# def cross_validate_tfidf():
-#     """ Referenced implementation: https://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html """
-#
-#     df_train = pd.read_csv("split_tfidf/1/training_data.csv")  # FIXME: Only using 1st K-Fold. Use all later when working
-#     df_test = pd.read_csv("split_tfidf/1/test_data.csv")
-#
-#     X_train = df_train.iloc[:, :-2]
-#     y_train = df_train.iloc[:, -1]
-#     X_test = df_test.iloc[:, :-2]
-#     y_test = df_test.iloc[:, -1]
-#
-#     y_train = label_binarize(y_train, classes=[1, 2, 3, 4, 5])
-#     y_test = label_binarize(y_test, classes=[1, 2, 3, 4, 5])
-#
-#     model = build_model(2, 4, X_train.shape[1], 5, clipnorm=1.0)
-#     # FIXME: NaN values when you put smaller batchsize / more epoches. y_score explodes in value, not sure why?
-#     # FIXME: However, clipnorm should remove them, so I'm not sure why it stil explodes
-#     history = model.fit(X_train, y_train, epochs=32, batch_size=32, verbose=2)
-#     y_score = model.predict(X_test)
-#
-#     fpr = dict()
-#     tpr = dict()
-#     roc_auc = dict()
-#     for i in range(5):
-#         fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
-#         roc_auc[i] = auc(fpr[i], tpr[i])
-#
-#     plotROC(fpr, tpr, roc_auc)
